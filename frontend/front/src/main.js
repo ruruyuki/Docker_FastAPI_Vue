f7ed73ae919787562377fc4089f078a9b8631ecf
yuki <github>
2022-02-20 17:48:24 +0900

    構築初回

---
+++ frontend/front/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import AxiosPlugin from './plugins/axios'
+
+createApp(App).use(VueAxios, axios)
+              .use(AxiosPlugin)
+              .mount('#app')
+              
